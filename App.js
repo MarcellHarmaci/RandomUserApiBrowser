c1ed02f21e7b73cd53f2cfb15dcf0372b9d45aa9
Marcell Harmaci <harmaci.marci@gmail.com>
2021-05-31 19:09:42 +0200

    Add a list with static demo data

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <FlatList
+                scrollEnabled={true}
+                data={[
+                    { key: "a", name: 'Devin' },
+                    { key: "b", name: 'Dan' },
+                    { key: "c", name: 'Dominic' },
+                    { key: "d", name: 'Jackson' },
+                    { key: "e", name: 'James' },
+                    { key: "f", name: 'Joel' },
+                    { key: "g", name: 'John' },
+                    { key: "h", name: 'Jillian' },
+                    { key: "i", name: 'Jimmy' },
+                    { key: "j", name: 'Julie' },
+                    { key: "k", name: 'David' },
+                    { key: "l", name: 'Danny' },
+                    { key: "m", name: 'Darla' },
+                ]}
+                renderItem={({ item }) =>
+                    <View style={styles.item}>
+                        <Image
+                            source={{ uri: 'https://reactnative.dev/docs/assets/p_cat1.png' }}
+                            style={{ width: 50, height: 50 }}
+                        />
+                        <Text style={styles.item}>
+                            {item.name}
+                        </Text>
+                    </View>
+                }
+            />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        padding: 8,
+        paddingTop: 16,
+        backgroundColor: '#fff',
+        alignItems: 'stretch',
+        justifyContent: 'center',
+    },
+    item: {
+        width: '100%',
+        flexDirection: 'row',
+        padding: 10,
+        fontSize: 18,
+        height: 70,
+    },
 });
