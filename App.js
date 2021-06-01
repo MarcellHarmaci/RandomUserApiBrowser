796924edb5f374bd3392364c63a0ff8218412095
Marcell Harmaci <harmaci.marci@gmail.com>
2021-06-01 11:34:16 +0200

    Add navigation on list item click

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { Image, FlatList, StyleSheet, Text, View } from 'react-native';
+import { Image, FlatList, StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function NavigationStack() {
+    return (
+        <Stack.Navigator
+            initialRouteName="Home"
+            headerMode="screen"
+            screenOptions={{
+                headerTintColor: 'white',
+                headerStyle: { backgroundColor: 'lightseagreen' },
+            }}
+        >
+            <Stack.Screen
+                name="RandomPeople"
+                component={RandomPeople}
+
+            />
+            <Stack.Screen
+                name="PersonDetails"
+                component={PersonDetails}
+            />
+        </Stack.Navigator>
+    );
+}
+
+function RandomPeople({ navigation }) {
     const [users, setUsers] = useState([])
     const [isFetching, setIsFetching] = useState(false)
 
     useEffect(() => {
-        fetchUsersAsync()        
+        fetchUsersAsync()
     }, [])
 
     async function fetchUsersAsync() {
@@ -23,13 +50,16 @@ export default function App() {
 
     async function onRefresh() {
         setIsFetching(true)
-        setUsers([])
 
-        // TODO Fetch random users
+        setUsers([])
         await fetchUsersAsync()
 
         setIsFetching(false)
     }
+
+    const navigateToPerson = () => {
+        navigation.navigate("PersonDetails")
+    };
 
     return (
         <View style={styles.container}>
@@ -37,18 +67,20 @@ export default function App() {
                 data={users}
                 scrollEnabled={true}
                 refreshing={isFetching}
-                onRefresh={ () => onRefresh() }
-                keyExtractor={ (item) => item.login.uuid.toString() }
+                onRefresh={() => onRefresh()}
+                keyExtractor={(item) => item.login.uuid}
                 renderItem={({ item }) =>
-                    <View style={styles.item}>
-                        <Image
-                            source={{ uri: item.picture.medium }}
-                            style={{ width: 50, height: 50 }}
-                        />
-                        <Text style={styles.item}>
-                            {`${item.name.title} ${item.name.first} ${item.name.last}`}
-                        </Text>
-                    </View>
+                    <TouchableWithoutFeedback onPress={navigateToPerson}>
+                        <View style={styles.item}>
+                            <Image
+                                source={{ uri: item.picture.medium }}
+                                style={{ width: 50, height: 50 }}
+                            />
+                            <Text style={styles.item}>
+                                {`${item.name.title} ${item.name.first} ${item.name.last}`}
+                            </Text>
+                        </View>
+                    </TouchableWithoutFeedback>
                 }
             />
             <StatusBar style="auto" />
@@ -56,11 +88,28 @@ export default function App() {
     );
 }
 
+function PersonDetails({ navigation }) {
+    return (
+        <View>
+            <Text>
+                Hello world!
+            </Text>
+        </View>
+    );
+}
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <NavigationStack />
+        </NavigationContainer>
+    );
+}
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 8,
-        paddingTop: 24,
         backgroundColor: '#fff',
         alignItems: 'stretch',
         justifyContent: 'center',
@@ -68,7 +117,7 @@ const styles = StyleSheet.create({
     item: {
         width: '100%',
         flexDirection: 'row',
-        padding: 10,
+        padding: 8,
         fontSize: 18,
         height: 70,
     },
